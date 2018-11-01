58a43714b6f82067d72032cf9ec2595529060138
Yuan Wang <wang17@ualberta.ca>
2018-10-31 18:52:39 -0600

    update lab

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitt/ImportantLonelyTweet.java
@@ -0,0 +1,38 @@
+package ca.ualberta.cs.lonelytwitt;
+
+import android.util.Log;
+
+import java.util.Date;
+
+public class ImportantLonelyTweet extends LonelyTweet {
+
+	public ImportantLonelyTweet() {
+	}
+
+	public ImportantLonelyTweet(String text, Date date) {
+		this.tweetDate = date;
+		this.tweetBody = text;
+	}
+
+	@Override
+	public boolean isValid() {
+		if (tweetBody.trim().length() == 0
+				|| tweetBody.trim().length() > 10) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+
+		String string = getTweetDate() + " | " + getTweetBody() ;
+		Log.i("Important Tweet",string);
+		return string;
+	}
+
+	public String getTweetBody() {
+        return tweetBody.toUpperCase();
+    }
+}
